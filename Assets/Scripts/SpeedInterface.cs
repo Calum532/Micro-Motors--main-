e1d3c4f59850c4fd4a58128f28a2b2794cbcff01
Calum <43376276+Calum532@users.noreply.github.com>
2020-02-28 23:26:42 +0000

    test drive gamemode finished

---
+++ Assets/Scripts/SpeedInterface.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityStandardAssets.Vehicles.Car;
+
+public class SpeedInterface : MonoBehaviour
+{
+    public static float magnitudeSpeed;
+    public static double mphSpeed;
+
+    public GameObject SpeedUI;
+    GameObject Player;
+
+    void Update()
+    {
+        //if player has chosen a vehicle
+        if (ChooseRacer_testdrive.RacerChosen)
+        {
+            Player = GameObject.FindGameObjectWithTag("Player");
+            Player.GetComponent<CarUserControl>().enabled = true;
+
+            magnitudeSpeed = Player.GetComponent<Rigidbody>().velocity.magnitude;
+            mphSpeed = magnitudeSpeed * 2.222;
+            SpeedUI.GetComponent<TextMeshProUGUI>().text = mphSpeed.ToString("F0") + "mph";
+        }
+    }
+}
