0d365bb37acdd8f458feacd51c1e00f93ff677e4
Lee <L918Sturrock@hotmail.com>
2020-03-11 23:24:16 +0000

    Added colour change lerping to the speed ui element depending on the current player speed.

--- Assets/Micro Motors Assets/Scripts/Test drive/SpeedInterface.cs
+++ Assets/Micro Motors Assets/Scripts/Test drive/SpeedInterface.cs
@@ -20,6 +20,10 @@ public class SpeedInterface : MonoBehaviour
 
             magnitudeSpeed = Player.GetComponent<Rigidbody>().velocity.magnitude;
             mphSpeed = magnitudeSpeed * 2.222;
+            // colour stuff
+            SpeedUI.GetComponent<TextMeshProUGUI>().color = Color.Lerp(SpeedUI.GetComponent<TextMeshProUGUI>().color, mphSpeed >= 30 ? Color.yellow : Color.white, Time.deltaTime * 2f);
+            SpeedUI.GetComponent<TextMeshProUGUI>().color = Color.Lerp(SpeedUI.GetComponent<TextMeshProUGUI>().color, mphSpeed >= 90 ? Color.red : Color.white, Time.deltaTime * 2f);
+            //
             SpeedUI.GetComponent<TextMeshProUGUI>().text = mphSpeed.ToString("F0") + "mph";
         }
     }
