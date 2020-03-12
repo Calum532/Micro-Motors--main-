8c80cc9ff48b5b1239b23326817d4010a0724323
Lee <L918Sturrock@hotmail.com>
2020-03-12 18:40:02 +0000

    Created vehicle collision related scripts

---
+++ Assets/Testing/Scripts/VehicleCollision.cs
@@ -0,0 +1,27 @@
+﻿
+using UnityEngine;
+
+public class VehicleCollision : MonoBehaviour
+{
+    [Header("Particle to play:")]
+    public ParticleSystem collisionParticles;
+
+    [Header("Settings")]
+    public int ForceRequired;
+
+    private ParticleSystem _cachedParticles;
+    private GameObject _this;
+
+    void Awake()
+    {
+        _this = gameObject;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {       
+        if (collision.relativeVelocity.magnitude > ForceRequired)
+        {
+            _cachedParticles = Instantiate(collisionParticles, _this.transform.position, collision.transform.rotation);
+        }
+    }
+}
---
+++ Assets/Testing/Scripts/VehicleCollisionSound.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class VehicleCollisionSound : MonoBehaviour
+{
+    [Header("Collision Sounds")]
+    public AudioClip[] collisionImpacts;
+
+    [Header("Settings")]
+    public float playAfter;
+    public float deleteAfter;
+
+    private AudioSource _source;
+    private GameObject _this;
+
+    void Awake()
+    {
+        if (deleteAfter <= 0f)
+        {
+            deleteAfter = 1f;
+        }
+        _source = GetComponent<AudioSource>();
+        _this = gameObject;
+    }
+
+    void Start()
+    {
+        _source.pitch = (Random.Range(0.95f, 1.05f));
+        _source.clip = collisionImpacts[Random.Range(0, collisionImpacts.Length)];
+        StartCoroutine(ExecuteAfterTime(playAfter));
+    }
+
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        // Code to execute after the delay
+        _source.PlayOneShot(_source.clip);
+        Destroy(_this, deleteAfter);
+    }
+}
