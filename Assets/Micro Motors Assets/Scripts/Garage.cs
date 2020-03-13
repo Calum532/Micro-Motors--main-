9e789d0d7f1761b39a87c7301adb06601a5e895a
Lee <L918Sturrock@hotmail.com>
2020-03-13 00:02:48 +0000

    Fixed vehicle select load times, updated vehicle colliders, implemented vehicle collison sounds

--- Assets/Micro Motors Assets/Scripts/Garage.cs
+++ Assets/Micro Motors Assets/Scripts/Garage.cs
@@ -7,12 +7,12 @@ using UnityEngine.UI;
 public class Garage : MonoBehaviour
 {
     public static bool RCOwned = true;
-    public static bool UFOOwned = false;
-    public static bool TaxiOwned = false;
+    public static bool UFOOwned = true;
+    public static bool TaxiOwned = true;
     public static bool TruckOwned = true;
-    public static bool TankOwned = false;
-    public static bool MonsterOwned = false;
-    public static bool DozerOwned = false;
+    public static bool TankOwned = true;
+    public static bool MonsterOwned = true;
+    public static bool DozerOwned = true;
     public static bool SkyCarOwned = true;
 
     public float RCCost;
