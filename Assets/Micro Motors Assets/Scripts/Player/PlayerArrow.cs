bd577a57cbbdb5205f5b27b93ea8d06f72496301
Lee <L918Sturrock@hotmail.com>
2020-04-11 01:14:52 +0100

    Utilised a lerp function to make the arrow more pleasing to look at and not move so jarringly, did some code refactoring

--- Assets/Micro Motors Assets/Scripts/Player/PlayerArrow.cs
+++ Assets/Micro Motors Assets/Scripts/Player/PlayerArrow.cs
@@ -1,18 +1,31 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerArrow : MonoBehaviour
 {
+    [Header("Objects:")]
     public GameObject Arrow;
     public GameObject WaypointTargetObject;
-    bool isActive = true;
+
+    [Header("Settings")]
+    public float lerpSpeed;
+
     //public GameObject WrongWayUI;
+
+    private bool isActive = true;
+
+    void Awake()
+    {
+        if (lerpSpeed <= 0)
+        {
+            lerpSpeed = 2f;
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
-        Arrow.transform.LookAt(WaypointTargetObject.transform.position);
+        Arrow.transform.rotation = Quaternion.Lerp(Arrow.transform.rotation, WaypointTargetObject.transform.rotation, lerpSpeed * Time.deltaTime);
+
         if (Input.GetKeyDown(KeyCode.F))
         {
             if (isActive)
