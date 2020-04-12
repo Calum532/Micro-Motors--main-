7d39cc28ed2a6dd85fcb1ae88fc40c4b0e91791b
Lee <L918Sturrock@hotmail.com>
2020-04-12 15:28:06 +0100

    Created 2 new mystery box effects: Slam Dunk and Super Boost, added relevant audio for mystery box effects, did some coder refactoring , implemented better debugging

--- Assets/Micro Motors Assets/Scripts/Standard Race/PowerUp.cs
+++ Assets/Micro Motors Assets/Scripts/Standard Race/PowerUp.cs
@@ -12,12 +12,23 @@ public class PowerUp : MonoBehaviour
     //public GameObject FrontCam;
     //public GameObject ZoomedOutCam;
 
+    [Header("3D audio")]
+    public GameObject Swoosh;
+    public GameObject SuperBoost;
+    public GameObject Dunk;
+
+    [Header("Particles/Effects")]
+    public GameObject placeholder;
+
+    [Header("Settings")]
     public float respawnDuration = 5f;
     public float effectDuration = 10f;
     public float grow;
     public float rotateSpeed;
 
     private int randomNum;
+
+    private GameObject _cache;
 
     private void Update()
     {
@@ -33,11 +44,12 @@ public class PowerUp : MonoBehaviour
                 FindObjectOfType<AudioManager>().Play("PickUp");
             }
 
-            Instantiate(pickupEffect, transform.position, transform.rotation);
+            _cache = Instantiate(pickupEffect, transform.position, transform.rotation);
+            Destroy(_cache, 2f);
+
             StartCoroutine(respawnCountdown());
 
-            randomNum = UnityEngine.Random.Range(0, 1);
-            Debug.Log("Random number generated: " + randomNum);
+            randomNum = UnityEngine.Random.Range(0, 3);           
 
             if (randomNum == 0)
             {
@@ -45,7 +57,11 @@ public class PowerUp : MonoBehaviour
             }
             else if (randomNum == 1)
             {
-                StartCoroutine(PickupGrow(other));
+                StartCoroutine(PickupSuperBoost(other));
+            }
+            else if (randomNum == 2)
+            {
+                StartCoroutine(PickupSlamDunk(other));
             }
         }
     }
@@ -71,6 +87,15 @@ public class PowerUp : MonoBehaviour
 
     IEnumerator PickupGrow(Collider racer)
     {
+        if (racer.CompareTag("Player"))
+        {
+            Debug.Log($"<color=cyan>{racer.transform.name}: got 'Grow' pickup!</color>");
+        }
+        else
+        {
+            Debug.Log($"<color=yellow>{racer.transform.name}: got 'Grow' pickup!</color>");
+        }      
+
         FindObjectOfType<AudioManager>().Play("Buff");
         //ZoomedOutCam.SetActive(true);
         //FrontCam.SetActive(false);
@@ -86,6 +111,15 @@ public class PowerUp : MonoBehaviour
 
     IEnumerator PickupShrink(Collider racer)
     {
+        if (racer.CompareTag("Player"))
+        {
+            Debug.Log($"<color=cyan>{racer.transform.name}: got 'Shrink' pickup!</color>");
+        }
+        else
+        {
+            Debug.Log($"<color=yellow>{racer.transform.name}: got 'Shrink' pickup!</color>");
+        }
+
         FindObjectOfType<AudioManager>().Play("Debuff");
         racer.transform.localScale /= grow;
 
@@ -93,5 +127,62 @@ public class PowerUp : MonoBehaviour
 
         racer.transform.localScale *= grow;
         FindObjectOfType<AudioManager>().Play("Buff");
+    }
+
+    IEnumerator PickupSlamDunk(Collider racer)
+    {
+        // debugging
+        if (racer.CompareTag("Player"))
+        {
+            Debug.Log($"<color=cyan>{racer.transform.name}: got 'Slam Dunk' pickup!</color>");
+        }
+        else
+        {
+            Debug.Log($"<color=yellow>{racer.transform.name}: got 'Slam Dunk' pickup!</color>");
+        }
+
+        //audio
+        _cache = Instantiate(Swoosh, racer.transform.position, Quaternion.identity);
+        _cache.transform.parent = racer.transform;
+        Destroy(_cache, 2f);
+
+        //particles/effects
+
+        //implementation
+        racer.GetComponent<Rigidbody>().AddExplosionForce(1000f, racer.transform.position, 10f, 5f, ForceMode.Acceleration);
+
+        yield return new WaitForSeconds(1f);
+
+        //audio
+        _cache = Instantiate(Dunk, racer.transform.position, Quaternion.identity);
+        _cache.transform.parent = racer.transform;
+        Destroy(_cache, 2f);
+
+        racer.GetComponent<Rigidbody>().AddExplosionForce(-10000f, racer.transform.position, 10f, 5f, ForceMode.Acceleration);
+    }
+
+    IEnumerator PickupSuperBoost(Collider racer)
+    {
+        // debugging
+        if (racer.CompareTag("Player"))
+        {
+            Debug.Log($"<color=cyan>{racer.transform.name}: got 'Super Boost' pickup!</color>");
+        }
+        else
+        {
+            Debug.Log($"<color=yellow>{racer.transform.name}: got 'Super Boost' pickup!</color>");
+        }
+
+        //audio
+        _cache = Instantiate(SuperBoost, racer.transform.position, Quaternion.identity);
+        _cache.transform.parent = racer.transform;
+        Destroy(_cache, 3f);
+
+        // particles/effects
+
+        // implementation
+        racer.GetComponent<Rigidbody>().AddForce(racer.transform.forward * 1350000f);
+
+        yield return new WaitForSeconds(2f);
     }
 }
