84233eacc0123088954c0880d71b471fb599980f
Lee <L918Sturrock@hotmail.com>
2020-04-14 16:13:16 +0100

    Code refactoring, checking if variable is null to save console errors being spammed

--- Assets/Micro Motors Assets/Scripts/Player/Positioning/PlayerPos.cs
+++ Assets/Micro Motors Assets/Scripts/Player/Positioning/PlayerPos.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using TMPro;
 
 public class PlayerPos : MonoBehaviour
@@ -15,25 +13,28 @@ public class PlayerPos : MonoBehaviour
 
     void Update()
     {
-        if(playerPosition == 8 || playerPosition == 7 || playerPosition == 6 || playerPosition == 5 || playerPosition == 4)
+        if (PositionDisplayChild != null)
         {
-            PositionDisplayChild.GetComponent<TextMeshProUGUI>().text = "th";
-            PositionDisplayChild.GetComponent<TextMeshProUGUI>().color = new Color32(255, 255, 255, 255);
-        }
-        else if (playerPosition == 3)
-        {
-            PositionDisplayChild.GetComponent<TextMeshProUGUI>().text = "rd";
-            PositionDisplayChild.GetComponent<TextMeshProUGUI>().color = new Color32(176, 141, 87, 255);
-        }
-        else if (playerPosition == 2)
-        {
-            PositionDisplayChild.GetComponent<TextMeshProUGUI>().text = "nd";
-            PositionDisplayChild.GetComponent<TextMeshProUGUI>().color = new Color32(170, 169, 173, 255);
-        }
-        else if (playerPosition == 1)
-        {
-            PositionDisplayChild.GetComponent<TextMeshProUGUI>().text = "st";
-            PositionDisplayChild.GetComponent<TextMeshProUGUI>().color = new Color32(211, 175, 55, 255);
+            if (playerPosition == 8 || playerPosition == 7 || playerPosition == 6 || playerPosition == 5 || playerPosition == 4)
+            {
+                PositionDisplayChild.GetComponent<TextMeshProUGUI>().text = "th";
+                PositionDisplayChild.GetComponent<TextMeshProUGUI>().color = new Color32(255, 255, 255, 255);
+            }
+            else if (playerPosition == 3)
+            {
+                PositionDisplayChild.GetComponent<TextMeshProUGUI>().text = "rd";
+                PositionDisplayChild.GetComponent<TextMeshProUGUI>().color = new Color32(176, 141, 87, 255);
+            }
+            else if (playerPosition == 2)
+            {
+                PositionDisplayChild.GetComponent<TextMeshProUGUI>().text = "nd";
+                PositionDisplayChild.GetComponent<TextMeshProUGUI>().color = new Color32(170, 169, 173, 255);
+            }
+            else if (playerPosition == 1)
+            {
+                PositionDisplayChild.GetComponent<TextMeshProUGUI>().text = "st";
+                PositionDisplayChild.GetComponent<TextMeshProUGUI>().color = new Color32(211, 175, 55, 255);
+            }
         }
     }
 }
